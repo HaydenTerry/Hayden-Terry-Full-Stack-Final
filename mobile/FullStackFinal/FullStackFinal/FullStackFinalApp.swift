c851159c41b6dbec8137274512c6af518bcad773
Hayden Terry <hayden.terry@nwktc.edu>
2024-05-07 10:24:50 -0600

    Updated file paths

---
+++ mobile/FullStackFinal/FullStackFinal/FullStackFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullStackFinalApp.swift
+//  FullStackFinal
+//
+//  Created by Hayden Terry on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FullStackFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
