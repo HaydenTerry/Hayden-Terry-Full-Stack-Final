ffc1b17c94afa09ed4232460166abda1a467f0a7
Hayden Terry <hayden.terry@nwktc.edu>
2024-05-07 13:26:17 -0600

    backend setup

---
+++ backend/backend.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+     
+})
+
+app.get('/', (request, response) => {
+     
+})
+
+app.listen(3000, () => {
+    console.log(`Server is Listening on 3000`)
+})
