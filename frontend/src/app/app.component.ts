b853d835ec1e221a857140309b33958f101d0c51
Hayden Terry <hayden.terry@nwktc.edu>
2024-05-09 11:01:37 -0600

    Auto stash before merge of "main" and "adminpanel"

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 // import { ethers } from 'ethers';
 import { SignInService } from './sign-in.service';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AdminPanelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
